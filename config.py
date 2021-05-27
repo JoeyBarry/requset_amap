d77293c0ed0b428fe4fb0e017cedca76e39f009a
joeybarry <joeybarry@163.com>
2021-05-27 16:37:48 +0800

    基本参数设置

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-key = ''    #高德Web API
+key = ''    #高德Web API 
 url_data = 'https://restapi.amap.com/v3/place/around?key=' + key
 url_road = 'https://restapi.amap.com/v3/traffic/status/road?adcode=440100&key=' + key
 url_photo = 'https://restapi.amap.com/v3/staticmap?location=113.324220,23.128692&zoom=16&size=960*720&scale=2&key=' + key
